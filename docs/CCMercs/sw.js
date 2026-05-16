aceae9294e52f22986e970994c58a52440b6c2da
stonewall072 <jacob.i.wall@gmail.com>
2026-05-16 09:10:30 -0400

    Change legacy pilots to Nameless Pilots

--- docs/CCMercs/sw.js
+++ docs/CCMercs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bt-ccmercs-v15'; // bump this
+const CACHE_NAME = 'bt-ccmercs-v21'; // bump this
 
 self.addEventListener('install', event => {
   self.skipWaiting(); // no pre-caching, let fetch handler build cache naturally
